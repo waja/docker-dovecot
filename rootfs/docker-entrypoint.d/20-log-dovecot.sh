7696454d32f392a86468d051f742c66e90630093
Jan Wagner <waja@cyconet.org>
2022-11-14 21:16:08 +0100

    Add config modifications

---
+++ rootfs/docker-entrypoint.d/20-log-dovecot.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Set logging to STDOUT/STDERR
+sed -i -e 's,#log_path =,log_path = /dev/stderr,' \
+          -e 's,#info_log_path =,info_log_path = /dev/stdout,' \
+          -e 's,#debug_log_path =,debug_log_path = /dev/stdout,' \
+       /etc/dovecot/conf.d/10-logging.conf
