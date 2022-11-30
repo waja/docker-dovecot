c1955484395cf2e8e0888addc78f0575f93b5374
Jan Wagner <waja@cyconet.org>
2022-11-30 10:48:21 +0000

    Ajust our query to prefer STARTTLS

--- rootfs/docker-entrypoint.d/30-local-config-dovecot.sh
+++ rootfs/docker-entrypoint.d/30-local-config-dovecot.sh
@@ -25,6 +25,6 @@ if [ ! -f /etc/dovecot/dovecot-sqlite.conf.ext ]; then
 	cat > /etc/dovecot/dovecot-sqlite.conf.ext << EOF
 driver = sqlite
 connect = /var/lib/dovecot/sqlite/dct.db
-password_query = SELECT NULL AS password, 'Y' as nopassword, host, destuser, 'Y' AS proxy FROM proxy WHERE user = '%u'
+password_query = SELECT NULL AS password, "yes" as starttls, 'Y' as nopassword, host, destuser, 'Y' AS proxy FROM proxy WHERE user = '%u'
 EOF
 fi
