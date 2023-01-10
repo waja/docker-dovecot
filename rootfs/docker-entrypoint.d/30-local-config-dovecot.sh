e8fd81f14509cb948bb2b9cc5bd45acf7c7d0f83
Jan Wagner <waja@cyconet.org>
2023-01-10 09:18:22 +0000

    Add some more config comments and enable the protocols imap lmtp sieve pop3 explicit

--- rootfs/docker-entrypoint.d/30-local-config-dovecot.sh
+++ rootfs/docker-entrypoint.d/30-local-config-dovecot.sh
@@ -14,5 +14,12 @@ passdb {
   driver = sql
   args = /etc/dovecot/dovecot-sqlite.conf.ext
 }
+
+#auth_policy_request_attributes = login=%{requested_username} pwhash=%{hashed_password} remote=%{rip} device_id=%{client_id} protocol=%s
+protocols = imap lmtp sieve pop3
+#ssl = yes
+#ssl_cipher_list = ALL:!kRSA:!SRP:!kDHd:!DSS:!aNULL:!eNULL:!EXPORT:!DES:!3DES:!MD5:!PSK:!RC4:!ADH:!LOW@STRENGTH
+#ssl_min_protocol = TLSv1
+#ssl_prefer_server_ciphers = no
 EOF
 fi
