770055984750932317f57179636fbf3867bad616
Jan Wagner <waja@cyconet.org>
2024-04-23 22:07:40 +0200

    Fix formating

--- rootfs/docker-entrypoint.d/20-log-dovecot.sh
+++ rootfs/docker-entrypoint.d/20-log-dovecot.sh
@@ -2,6 +2,6 @@
 
 # Set logging to STDOUT/STDERR
 sed -i -e 's,#log_path =,log_path = /dev/stderr,' \
-          -e 's,#info_log_path =,info_log_path = /dev/stdout,' \
-          -e 's,#debug_log_path =,debug_log_path = /dev/stdout,' \
-       /etc/dovecot/conf.d/10-logging.conf
+	-e 's,#info_log_path =,info_log_path = /dev/stdout,' \
+	-e 's,#debug_log_path =,debug_log_path = /dev/stdout,' \
+	/etc/dovecot/conf.d/10-logging.conf
--- rootfs/docker-entrypoint.d/31-local-cert-dovecot.sh
+++ rootfs/docker-entrypoint.d/31-local-cert-dovecot.sh
@@ -5,7 +5,7 @@ if [ -n "${CERT_CN}" ]; then
 	[ -L /etc/letsencrypt/live/"${CERT_CN}"/privkey.pem ] && [ ! -L /etc/ssl/dovecot/"${CERT_CN}".key ] && ln -s /etc/letsencrypt/live/"${CERT_CN}"/privkey.pem /etc/ssl/dovecot/"${CERT_CN}".key
 	if [ -L /etc/ssl/dovecot/"${CERT_CN}".key ] && [ -L /etc/ssl/dovecot/"${CERT_CN}".pem ]; then
 		# Create a sane local.conf
-		cat >> /etc/dovecot/local.conf << EOF
+		cat >>/etc/dovecot/local.conf <<EOF
 # Adjust certificates to our needs
 ssl_cert = </etc/ssl/dovecot/${CERT_CN}.pem
 ssl_key = </etc/ssl/dovecot/${CERT_CN}.key
